51b1685ea042d1826f9c2a39a7884ed778bba94e
Rob Griffin <robgriffin247@hotmail.com>
2026-02-27 13:23:38 +0100

    fixed timestamp in results

--- apps/ittea_and_scone/pages/main.py
+++ apps/ittea_and_scone/pages/main.py
@@ -162,7 +162,7 @@ with tab_results:
             "rider":st.column_config.TextColumn("Rider"),
             "category":st.column_config.TextColumn("Cat."),
             "stage_name":st.column_config.TextColumn("Route"),
-            "event_start_datetime":st.column_config.DatetimeColumn("Date/Time", format="D/M/YY HH:MM"),
+            "event_start_datetime":st.column_config.DatetimeColumn("Date/Time", format="D/M/YY HH:mm"),
             "race_time":st.column_config.TextColumn("Time"),
             "race_speed":st.column_config.NumberColumn("Speed", format="%.2f"),
             "is_best_effort":st.column_config.CheckboxColumn("Best"),
